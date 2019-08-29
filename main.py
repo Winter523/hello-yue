b05abfbd869ccde7c4aada3067015ec7d43b1394
zhangxiaolong07 <zhangxiaolong07>
2019-08-29 17:12:11 +0800

    first add sth.

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf8
+"""
+A hello world program
+"""
+import os
+import sys
+
+def main():
+    print >> sys.stdout, "Hello yue!"
+
+
+if __name__ == "__main__":
+    main()
